2eb734d055c0b5f0f03407ab928125ee75ad9771
TURKCELL\EXT02D22125 <omer.ersoy@kgteknoloji.com>
2020-10-04 16:43:37 +0300

    fixed: wrong swipe direction issue.

--- util/helpers/GestureHelper.js
+++ util/helpers/GestureHelper.js
@@ -1,6 +1,8 @@
 import { Directions } from 'react-native-gesture-handler'
+import { Dimensions } from 'react-native'
 
 export const getSwipeDirection = (event, width) => {
     const {x} = event
-    return x < width / 2 ? Directions.LEFT : Directions.RIGHT
+    let actualWidth = width === "100%" ? Dimensions.get("screen").width : width
+    return x < actualWidth / 2 ? Directions.LEFT : Directions.RIGHT
 }
